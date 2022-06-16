2fe7da7ebdb3bdd207a173f698318b6956cc07c2
G0nov4 <62616102+G0nov4@users.noreply.github.com>
2022-06-16 03:25:40 -0400

    Create Fibo_recursivo.fs
    agregado Fibo en F#

---
+++ P1/Fibo_recursivo.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let rec fib n =
+    match n with
+    | 1 -> 0
+    | 2 | 3 -> 1
+    | n -> fib(n-1) + fib(n-2) + fib(n-3)
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Ingrese un numero: ")
+    let n = Console.ReadLine();
+    for i in 1 .. int(n) do
+        Console.WriteLine(fib(i))
+    0;
